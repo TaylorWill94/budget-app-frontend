d41813b2a1c8d63d94055cfc11f91d6af387e118
Taylor W <taylorwilliams@pursuit.org>
2022-01-23 16:09:29 -0500

    Updated CSS

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,17 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <footer className="footer">
-            Contact
-        <Link to ='/'><p>Home</p></Link>
+  return (
+    <footer className="footer">
+      Contact
+      <Link to="/">
+        <p>Home</p>
+      </Link>
+      <a href="https://www.linkedin.com/in/taylor-williams-a0683795/">
         <p>GitHub</p>
-        <p>Linkedin</p>
-        </footer>
-    )
-}
+      </a>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
