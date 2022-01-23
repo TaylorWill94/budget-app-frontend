c16aa189f2fa8b43afc3a403d2a037df08ea28b1
Taylor W <taylorwilliams@pursuit.org>
2022-01-23 18:21:33 -0500

    Updated CSS

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="home">
-      <h1>Welcome To The Budget Bae</h1>
+      <h1>Welcome To Budget Bae</h1>
       <h2>
         Budget Bae allows you to organize and track all your expenses in one
         place.
       </h2>
       <h2>Connect with Budget Bae today!</h2>
       <Link to="/transactions">
-        <button className="nav-items">Take me to my budgets</button>
+        <button className="nav-items begin">Let's begin!</button>
       </Link>
     </div>
   );
