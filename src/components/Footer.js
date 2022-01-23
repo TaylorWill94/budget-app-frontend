bb0f2fee4d72d433c2f07f050f9f2b2743aa7345
Taylor W <taylorwilliams@pursuit.org>
2022-01-23 14:39:01 -0500

    Added a redirect folder

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,12 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div className="footer">
+        <footer className="footer">
             Contact
         <Link to ='/'><p>Home</p></Link>
         <p>GitHub</p>
         <p>Linkedin</p>
-        </div>
+        </footer>
     )
 }
 
